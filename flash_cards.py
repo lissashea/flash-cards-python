207522c55679ee9fcba74a67df0e427f4a1b65ed
“Lissa <lwarshaw674@gmail.com>
2023-05-11 14:25:19 -0400

    updated loop

--- flash_cards.py
+++ flash_cards.py
@@ -8,3 +8,19 @@ def create_card(front,back):
     print("Flashcard created successfully")
 
 def review_cards(num_cards):
+    cards = list(FlashCard.select())
+    random.shuffle(cards)
+    review_count = min(num_cards, len(cards))
+
+    for i in range(review_count):
+        card = cards[i]
+        print(f"Front: {card.front}")
+        input("Press Enter to see the back...")
+        print(f"Back: {card.back}")
+
+        answer = input("Did you answer correctly? (y/n): ")
+        if answer.lower() == 'y':
+            card.correct_count += 1
+        else:
+            card.incorrect_count += 1
+        card.save()
