4358a435278ffc897904df9f7b8f71c76713b65d
“Lissa <lwarshaw674@gmail.com>
2023-05-12 05:33:35 -0400

    installed dependencies

--- flash_cards.py
+++ flash_cards.py
@@ -24,3 +24,29 @@ def review_cards(num_cards):
         else:
             card.incorrect_count += 1
         card.save()
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Flash Cards Application")
+    subparsers = parser.add_subparsers(dest="command")
+
+    create_parser = subparsers.add_parser("create", help="Create a new flash card")
+    create_parser.add_argument("front", help="Front side of the flash card")
+    create_parser.add_argument("back", help="Back side of the flash card")
+
+    review_parser = subparsers.add_parser("review", help="Set up a training session and review flash cards")
+    review_parser.add_argument("num_cards", type=int, help="Number of flash cards to review")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    create_tables()
+
+    args = parse_arguments()
+
+    if args.command == "create":
+        create_card(args.front, args.back)
+    elif args.command == "review":
+        review_cards(args.num_cards)
+    else:
+        print("Invalid command")
