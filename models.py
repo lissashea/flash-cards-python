7d9a4ba71225350fced55014507c79eb6e3559b5
“Lissa <lwarshaw674@gmail.com>
2023-05-15 11:10:23 -0400

    updated hw

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 from peewee import *
 
-# Modify the connection parameters to match your PostgreSQL setup
 db = PostgresqlDatabase('flash_cards', 
                         user='lissa', 
                         password='123',
                         host='localhost', 
                         port=5432)
+
+db.connect()
 
 class FlashCard(Model):
     front = CharField()
@@ -16,13 +17,10 @@ class FlashCard(Model):
     class Meta:
         database = db
 
+
+
 def create_tables():
     with db:
         db.create_tables([FlashCard])
 
-# Add this code to create the database if it doesn't exist
-if not db.connect():
-    db.create_database('flash_cards')
-
-# Call the create_tables function to create the necessary tables
-create_tables()
+db.create_tables([FlashCard])
---
+++ settings.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE 'flash_cards';
